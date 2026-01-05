f9fb10659965c7e530ba7f812633cecf8f75cde5
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2026-01-06 00:02:10 +0900

    client.tsの内容を修正 backend.tsにてタグ設定について追加

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,16 +1,4 @@
 import { generateClient } from 'aws-amplify/api';
 import type { Schema } from '../../amplify/data/resource';
 
-let _client: ReturnType<typeof generateClient<Schema>> | null = null;
-
-export const client = new Proxy(
-	{} as ReturnType<typeof generateClient<Schema>>,
-	{
-		get(target, prop) {
-			if (!_client) {
-				_client = generateClient<Schema>();
-			}
-			return Reflect.get(_client, prop);
-		},
-	},
-);
+export const client = generateClient<Schema>();
