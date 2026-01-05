66bc3208b0d5e0e13a9bc304a9dd3b748847f46b
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2026-01-05 19:58:44 +0900

    BedrockAPI送信の仕様について修正

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,16 @@
 import { generateClient } from 'aws-amplify/api';
 import type { Schema } from '../../amplify/data/resource';
 
-export const client = generateClient<Schema>();
+let _client: ReturnType<typeof generateClient<Schema>> | null = null;
+
+export const client = new Proxy(
+	{} as ReturnType<typeof generateClient<Schema>>,
+	{
+		get(target, prop) {
+			if (!_client) {
+				_client = generateClient<Schema>();
+			}
+			return Reflect.get(_client, prop);
+		},
+	},
+);
